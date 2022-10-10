281a8bc4bcc52248d746245e1b9dfb4f6a675bda
Thomas <65050898+tvonment@users.noreply.github.com>
2022-10-10 17:05:50 +0000

    add lockPermit

--- FormScripts/PermitFormFunctions.js
+++ FormScripts/PermitFormFunctions.js
@@ -37,5 +37,44 @@ ContosoPermit.Scripts.PermitForm = {
             }, function (error) { alert('Error:' + error.message) });
         }
     },
+    _lockPermitRequest: function (permitID, reason) {
+        this.entity = { entityType: "contoso_permit", id: permitID };
+        this.Reason = reason;
+        this.getMetadata = function () {
+            return {
+                boundParameter: "entity", parameterTypes: {
+                    "entity": {
+                        typeName: "mscrm.contoso_permit",
+                        structuralProperty: 5
+                    },
+                    "Reason": {
+                        "typeName": "Edm.String",
+                        "structuralProperty": 1 // Primitive Type
+                    }
+                },
+                operationType: 0, // This is an action. Use '1' for functions and '2' for CRUD
+                operationName: "contoso_LockPermit",
+            };
+        };
+    },
+    lockPermit: function (primaryControl) {
+        formContext = primaryControl;
+        var PermitID = formContext.data.entity.getId().replace('{', '').replace('}', '');
+        var lockPermitRequest = new ContosoPermit.Scripts.PermitForm._lockPermitRequest(PermitID, "Admin Lock");
+        // Use the request object to execute the function
+        Xrm.WebApi.online.execute(lockPermitRequest).then(
+            function (result) {
+                if (result.ok) {
+                    console.log("Status: %s %s", result.status, result.statusText);
+                    // perform other operations as required;
+                    formContext.ui.setFormNotification("Status " + result.status, "INFORMATION");
+                }
+            },
+            function (error) {
+                console.log(error.message);
+                // handle error conditions
+            }
+        );
+    },
     __namespace: true
 }
